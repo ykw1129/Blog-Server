d7637282df9a559e47886442ee35aca8338cb233
Killian <killian@8bsolutions.cn>
2022-08-11 21:56:52 +0800

    feat: add photo resolver

--- src/photo/photo.entity.ts
+++ src/photo/photo.entity.ts
@@ -1,22 +1,29 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+@ObjectType()
 @Entity()
 export class Photo {
+  @Field((type) => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field({ nullable: false })
   @Column({ length: 500 })
   name: string;
 
+  @Field({ nullable: true })
   @Column('text')
   description: string;
 
+  @Field({ nullable: true })
   @Column()
-  filename: string;
+  filename?: string;
 
+  @Field({ nullable: true })
   @Column('int')
   views: number;
 
+  @Field()
   @Column()
   isPublished: boolean;
 }
