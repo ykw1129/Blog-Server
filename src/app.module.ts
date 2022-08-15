18f8f1e4a0fe98fada0c9f1157fc5dd39098e887
Killian <killian@8bsolutions.cn>
2022-08-15 18:55:33 +0800

    feat: 修复 prettierrc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
-import { PhotoModule } from './photo/photo.module';
 import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { PhotoService } from './photo/photo.service';
-import { Photo } from './photo/photo.entity';
 
 @Module({
   imports: [
-    PhotoModule,
     DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
--- src/photo/photo.resolver.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: Killian killian@8bsolutions.cn
- * @Date: 2022-08-11 08:44:29
- * @LastEditors: Killian killian@8bsolutions.cn
- * @LastEditTime: 2022-08-11 08:44:38
- * @FilePath: \blog-server\src\photo\photo.resolver.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
