ae1011e8b512025808c910ab4c00ebeb4f2d50ce
ykw <1794093413@qq.com>
2022-09-02 07:10:45 +0800

    feat: 增加守卫和自定义装饰器

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -15,11 +15,13 @@ import {
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('/article')
 export class ArticleController {
   constructor(private readonly articlesService: ArticleService) {}
 
+  @Public()
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.articlesService.findAll(paginationQuery);
--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,10 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 Injectable();
 export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    return authHeader === process.env.API_KEY;
   }
 }
